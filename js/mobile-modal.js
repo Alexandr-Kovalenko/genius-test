f215dc18961bb4e8b5f93aa58ac355d037937706
Александр Коваленко <alexkovalenko121@gmail.com>
2024-02-08 12:51:36 +0200

    Correction modal window

--- js/mobile-modal.js
+++
@@ -1,6 +0,0 @@
-const mobileModal = document.querySelector(".backdrop");
-const mobileModalBtnOpen = document.querySelector(".mobile-modal-btn-open");
-
-const mobileToggleModal = () => modal.classList.toggle("is-hidden");
-
-mobileModalBtnOpen.addEventListener("click", mobileToggleModal);
